e1219b4bb5fc1a3f674cab28d5a7c94c7579452a
Volodymyr SAVCHENKO <contact@volodymyrsavchenko.com>
2020-04-13 19:23:18 +0200

    skeleton

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+import ast
+import sys
+
+setup_requires = ['setuptools >= 30.3.0', 'setuptools-git-version']
+
+if {'pytest', 'test', 'ptr'}.intersection(sys.argv):
+    setup_requires.append('pytest-runner')
+
+
+setup(description="oda-sparql",
+      long_description=open('README.md').read(),
+      version='0.4.8-dev0',
+      include_package_data=True,
+      setup_requires=setup_requires)
