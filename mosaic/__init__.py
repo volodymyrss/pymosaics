85b1a29410fb550ea43014dd811b7ee6658d9292
Volodymyr SAVCHENKO <contact@volodymyrsavchenko.com>
2020-04-14 09:50:40 +0200

    test data for ima

--- mosaic/__init__.py
+++ mosaic/__init__.py
@@ -9,6 +9,8 @@ import healpy
 
 import numpy as np
 
+debug = False
+
 def mosaic_fn_list(in_fn: List[str], out_fn: str, out_format: str, mock=False):
     print("input: %s output: %s"%( ", ".join(in_fn), out_fn))
 
@@ -30,12 +32,25 @@ class Mosaic:
         pass
 
     @staticmethod
-    def m(a, s_a, b, s_b):
+    def m(a, _s_a, b, _s_b):
         c=dict()
-        c['flux'] = s_a(a['flux']) + s_b(b['flux']) 
-        c['var'] = ( 1 / s_a(a['var']) + 1 / s_b(b['var']) ) ** -1
-        c['ex'] = s_a(a['ex']) + s_b(b['ex'])
-        c['n'] = s_a(a['n']) + s_b(b['n'])
+
+        _m = ~np.isnan(_s_b(a['flux']))
+
+        s_a = lambda x:_s_a(x)[_m]
+        s_b = lambda x:_s_b(x)[_m]
+
+        c['flux'] = a['flux']
+        c['var'] = a['var']
+        c['ex'] = a['ex']
+        c['n'] = a['n']
+
+        c['flux'][_m] = s_a(a['flux']) + s_b(b['flux']) 
+        c['var'][_m] = ( 1 / s_a(a['var']) + 1 / s_b(b['var']) ) ** -1
+        c['ex'][_m] = s_a(a['ex']) + s_b(b['ex'])
+        c['n'][_m] = s_a(a['n']) + s_b(b['n'])
+
+
         return c
 
     @staticmethod
@@ -92,6 +107,7 @@ class FITsMosaic(Mosaic):
                     (int(img['flux'].shape[0]/2)-10):(int(img['flux'].shape[0]/2)+10),
                     (int(img['flux'].shape[1]/2)-10):(int(img['flux'].shape[1]/2)+10),
                     ] = 100
+            fits.ImageHDU(img['flux'], header=img['wcs'].to_header()).writeto("mock-%s.fits"%fn.replace("/","_"), overwrite=True)
         
 
         if self.mosaic is None: 
@@ -100,11 +116,25 @@ class FITsMosaic(Mosaic):
         else:
             print("adding mosaic")
             m_j, m_i = np.meshgrid(
-                    np.arange(self.mosaic['flux'].shape[0]),
                     np.arange(self.mosaic['flux'].shape[1]),
+                    np.arange(self.mosaic['flux'].shape[0]),
                 )
+            #m_i=m_i[::-1,::-1]
+            #m_j=m_j[::-1,::-1]
 
-            m_ra, m_dec = self.mosaic['wcs'].wcs_pix2world(m_i, m_j, 0)
+            tm = m_i 
+            fits.ImageHDU(tm, header=self.mosaic['wcs'].to_header()).writeto("mi.fits", overwrite=True)
+            
+            tm = m_j 
+            fits.ImageHDU(tm, header=self.mosaic['wcs'].to_header()).writeto("mj.fits", overwrite=True)
+
+            tm = self.mosaic['flux']
+            fits.ImageHDU(tm, header=self.mosaic['wcs'].to_header()).writeto("tmf.fits", overwrite=True)
+            
+            tm = self.mosaic['flux'][m_i, m_j]
+            fits.ImageHDU(tm, header=self.mosaic['wcs'].to_header()).writeto("tmr.fits", overwrite=True)
+
+            m_ra, m_dec = self.mosaic['wcs'].wcs_pix2world(m_j, m_i, 0)
             i, j = img['wcs'].wcs_world2pix(m_ra, m_dec, 0)
             i = i.astype(int)
             j = j.astype(int)
@@ -112,27 +142,43 @@ class FITsMosaic(Mosaic):
             print("MOSAIC:", self.mosaic['wcs'])
             print("IMG:", img['wcs'])
 
-            tm = i - m_i
-            #tm = img['flux'][i, j]
-            fits.ImageHDU(tm, header=self.mosaic['wcs'].to_header()).writeto("t.fits", overwrite=True)
-
-            # or interpolate
 
             def pickornan(x, i, j):
-                f_x = x.flatten()
-                f_i = i.flatten()
-                f_j = k.flatten()
+                ij_usable = i>0
+                ij_usable &= j>0
+                ij_usable &= i<x.shape[0]
+                ij_usable &= j<x.shape[1]
 
-                m = f_i > 0
-                m &= f_i < x.shape[0] 
-                m &= f_j > 0
-                m &= f_j < x.shape[1] 
+                y = np.zeros_like(i, dtype=np.float32)
+                y[ij_usable] = x[i[ij_usable], j[ij_usable]]
+                y[~ij_usable] = float('nan')
+                return y
 
-                y = np.zeros_like(f_x)
+            tm = pickornan(self.mosaic['flux'], m_i, m_j)
+            fits.ImageHDU(tm - self.mosaic['flux'], header=self.mosaic['wcs'].to_header()).writeto("tmd.fits", overwrite=True)
 
-                y[m] = x[i, j].flatten()
-                y[~m.flatten()] = float('NaN')
-                return y.reshape(x.shape)
+            
+            tm = pickornan(self.mosaic['flux'], m_i, m_j)
+            fits.ImageHDU(tm, header=self.mosaic['wcs'].to_header()).writeto("tm.fits", overwrite=True)
+
+            if debug:
+                tm = pickornan(img['flux'], i, j)
+                fits.ImageHDU(tm, header=self.mosaic['wcs'].to_header()).writeto("t.fits", overwrite=True)
+
+                tm=np.zeros_like(i)
+                tm[ij_usable] = img['flux'][i[ij_usable], j[ij_usable]]
+                tm[~ij_usable] = -100
+                fits.ImageHDU(np.transpose(tm), header=self.mosaic['wcs'].to_header()).writeto("t.fits", overwrite=True)
+
+                tm = i 
+                tm[~ij_usable] = -100
+                #tm = img['flux'][i, j]
+                fits.ImageHDU(np.transpose(tm), header=self.mosaic['wcs'].to_header()).writeto("i.fits", overwrite=True)
+                
+                tm = j 
+                tm[~ij_usable] = -100
+                #tm = img['flux'][i, j]
+                fits.ImageHDU(np.transpose(tm), header=self.mosaic['wcs'].to_header()).writeto("j.fits", overwrite=True)
 
             self.mosaic.update(self.m(
                         self.mosaic,
@@ -143,7 +189,10 @@ class FITsMosaic(Mosaic):
 
 
     def writeto(self, fn):
-        fits.ImageHDU(self.mosaic['flux'], header=self.mosaic['wcs'].to_header()).writeto(fn, overwrite=True)
+        fits.HDUList([fits.PrimaryHDU()] + [
+            fits.ImageHDU(self.mosaic[k], header=self.mosaic['wcs'].to_header())
+                for k in self.mosaic if k != 'wcs'
+        ]).writeto(fn, overwrite=True)
 
 @click.command()
 @click.argument("in_fn", nargs=-1)
--- tests/test_mosaic.py
+++ tests/test_mosaic.py
@@ -2,10 +2,18 @@
 def test_healpy():
     pass
 
-def test_first():
+def test_first_mosaima():
     import mosaic
     out_fn = "out.fits"
     mosaic.mosaic_fn_list(["tests/data/isgri_mosa_ima_1.fits.gz", "tests/data/isgri_mosa_ima_2.fits.gz"], out_fn, "first", mock=True)
+
+    import os
+    assert os.path.exists(out_fn)
+
+def test_first_skyima():
+    import mosaic
+    out_fn = "out.fits"
+    mosaic.mosaic_fn_list(["tests/data/isgri_sky_ima_1.fits.gz", "tests/data/isgri_sky_ima_2.fits.gz"], out_fn, "first", mock=True)
 
     import os
     assert os.path.exists(out_fn)
