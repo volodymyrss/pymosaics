61572d8348e66192deb14236a4b9944578655007
Volodymyr SAVCHENKO <contact@volodymyrsavchenko.com>
2020-04-15 15:09:23 +0200

    Bump version: 0.3.2 → 0.3.3-dev0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 import ast
 import sys
 
-setup_requires = ['setuptools >= 30.3.2', 'setuptools-git-version']
+setup_requires = ['setuptools >= 30.3.3-dev0', 'setuptools-git-version']
 
 if {'pytest', 'test', 'ptr'}.intersection(sys.argv):
     setup_requires.append('pytest-runner')
@@ -10,6 +10,6 @@ if {'pytest', 'test', 'ptr'}.intersection(sys.argv):
 
 setup(description="pymosaic-fits",
       long_description=open('README.md').read(),
-      version='0.3.2',
+      version='0.3.3-dev0',
       include_package_data=True,
       setup_requires=setup_requires)
