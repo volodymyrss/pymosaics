51e85cc63bc5ca7efccb466f40506e1fabb3e61f
Carlo Ferrigno <ferrigno.carlo@gmail.com>
2020-06-09 14:27:05 +0200

    Increased version to 0.4.0 Introduced the update of keywords Removed some printouts

--- setup.py
+++ setup.py
@@ -10,6 +10,6 @@ if {'pytest', 'test', 'ptr'}.intersection(sys.argv):
 
 setup(description="pymosaic-fits",
       long_description=open('README.md').read(),
-      version='0.3.3',
+      version='0.4.0',
       include_package_data=True,
       setup_requires=setup_requires)
